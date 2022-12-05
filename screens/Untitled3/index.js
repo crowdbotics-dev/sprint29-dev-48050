98305c30629c99bc334e537a796de8db9b1e7972
Daniel Sousa <daniel.s@crowdbotics.com>
2022-12-05 18:29:27 +0000

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled3/index.js
@@ -0,0 +1,26 @@
+import { Text } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled3 = () => {
+  return <View style={_styles.VIoYSDmx}>
+      <Text style={_styles.OpIbXqfP}>Lorem ipsum…</Text></View>;
+};
+
+export default Untitled3;
+
+const _styles = StyleSheet.create({
+  VIoYSDmx: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  OpIbXqfP: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  }
+});
